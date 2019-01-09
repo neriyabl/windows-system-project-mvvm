be8088d19527aa46ad72c4d8fdbbfbfd6be66167
neriyabl <neriya_555@walla.com>
2019-01-09 19:22:09 +0200

    create report and Events Classes

---
+++ windows  system project/BE/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public double RealLAtitude { get; set; }
+        public double RealLongitude { get; set; }
+    }
+}
---
+++ windows  system project/BE/Report.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE
+{
+    public class Report
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Time { get; set; }
+        public int NoiseIntensity { get; set; }
+        public int NumOfExplosions { get; set; }
+        public int EventId { get; set; }
+    }
+}
