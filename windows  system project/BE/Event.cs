c07a1b2a885a9f01c60ca7285a98a30e60b51f0a
דניאל <danielrahimi0@gmail.com>
2019-01-10 15:40:21 +0200

    add dal project

--- windows  system project/BE/Event.cs
+++ windows  system project/BE/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,13 @@ namespace BE
 {
     public class Event
     {
+        [Key]
         public int Id { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
         public double RealLAtitude { get; set; }
         public double RealLongitude { get; set; }
+
+        public ICollection<Report> Reports { get; set; }
     }
 }
--- windows  system project/BE/Report.cs
+++ windows  system project/BE/Report.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace BE
 {
     public class Report
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Age { get; set; }
@@ -17,6 +19,6 @@ namespace BE
         public DateTime Time { get; set; }
         public int NoiseIntensity { get; set; }
         public int NumOfExplosions { get; set; }
-        public int EventId { get; set; }
+        public Event Event { get; set; }
     }
 }
