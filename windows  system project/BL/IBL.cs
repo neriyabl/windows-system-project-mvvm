cf130e64fb1c2960d0fd183a52e2d9b66d312395
neriyabl <neriya_555@walla.com>
2019-01-16 00:21:42 +0200

    create bl and factory dal

---
+++ windows  system project/BL/IBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace BL
+{
+    public interface IBl
+    {
+        #region Event methods
+        void AddEvent(Event _event);
+        void RemoveEvent(int id);
+        void UpdateEvent(Event _event);
+        List<Event> GetEvents(Predicate<Event> predicate = null);
+        Task<List<Event>> GetEventsAsync(Predicate<Event> predicate = null);
+        Event GetEvent(int? id);
+        #endregion
+
+        #region Report methods
+        void AddReport(Report report);
+        void RemoveReport(int? id);
+        void UpdateReport(Report report);
+        List<Report> GetReports(Predicate<Report> predicate = null);
+        Task<List<Report>> GetReportsAsync(Predicate<Report> predicate = null);
+        Report GetReport(int? id);
+        #endregion
+    }
+}
