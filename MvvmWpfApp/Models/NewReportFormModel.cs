34c55a9d06248f0b156535ce878087ee49fa9883
neriyabl <neriya_555@walla.com>
2019-02-11 21:13:10 +0200

    factory bl

--- MvvmWpfApp/Models/NewReportFormModel.cs
+++ MvvmWpfApp/Models/NewReportFormModel.cs
@@ -6,12 +6,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using BE;
+using BL;
 
 namespace MvvmWpfApp.Models
 {
     class NewReportFormModel: ICommand
     {
-        
+        private IBl bl = new FactoryBl().GetInstance();
 
         public Report Report { get; set; }
 
