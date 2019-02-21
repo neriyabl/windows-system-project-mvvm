d4cce9d069a4c8d7872f3d2dcddf5e630bf97a6e
neriyabl <neriya_555@walla.com>
2019-02-22 00:13:03 +0200

    refactor and updates on map view

---
+++ MvvmWpfApp/Views/MapView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Maps;
+using MvvmWpfApp.Annotations;
+
+namespace MvvmWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MapUC.xaml
+    /// </summary>
+    public partial class MapView : UserControl, INotifyPropertyChanged
+    {
+
+        public MapView()
+        {
+            InitializeComponent();
+            BingMap.Height = SystemParameters.PrimaryScreenHeight * 0.80;
+            BingMap.Width = SystemParameters.PrimaryScreenWidth * 0.70;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
