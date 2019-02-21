fe8c66038547f50e8b5b68e1c5895d91686283e2
neriyabl <neriya_555@walla.com>
2019-02-22 00:28:17 +0200

    logic in bl add report

--- BL/BlImp.cs
+++ BL/BlImp.cs
@@ -10,67 +10,85 @@ namespace BL
 {
     internal class BlImp : IBl
     {
+        private IDal _dal = new FactoryDal().GetInstance();
+
         #region Events
         public void AddEvent(Event _event)
         {
-            new FactoryDal().GetInstance().AddEvent(_event);
+            _dal.AddEvent(_event);
         }
 
         public void RemoveEvent(int id)
         {
-            new FactoryDal().GetInstance().RemoveEvent(id);
+            _dal.RemoveEvent(id);
         }
 
         public void UpdateEvent(Event _event)
         {
-            new FactoryDal().GetInstance().UpdateEvent(_event);
+            _dal.UpdateEvent(_event);
         }
 
         public List<Event> GetEvents(Predicate<Event> predicate = null)
         {
-            return new FactoryDal().GetInstance().GetEvents(predicate);
+            return _dal.GetEvents(predicate);
         }
 
         public Task<List<Event>> GetEventsAsync(Predicate<Event> predicate = null)
         {
-            return new FactoryDal().GetInstance().GetEventsAsync(predicate);
+            return _dal.GetEventsAsync(predicate);
         }
 
         public Event GetEvent(int? id)
         {
-            return new FactoryDal().GetInstance().GetEvent(id);
+            return _dal.GetEvent(id);
         }
         #endregion
 
         #region Reports
         public void AddReport(Report report)
         {
-            new FactoryDal().GetInstance().AddReport(report);
+            List<Event> events = GetEvents()
+                .Where(e => e.StartTime <= report.Time.AddMinutes(10) ||
+                 e.StartTime.AddMinutes(10) >= report.Time.AddMinutes(-10))
+                .ToList();
+            if (events.Count == 1)
+            {
+                //TODO check if need to change the event time
+            }
+            else if (events.Count > 1)
+            {
+                //TODO check whitch event contain the report time or which is the closest
+            }
+            else
+            {
+                report.Event = new Event {StartTime = report.Time};
+            }
+            _dal.AddReport(report);
         }
 
         public void RemoveReport(int id)
         {
-            new FactoryDal().GetInstance().RemoveReport(id);
+            _dal.RemoveReport(id);
         }
 
         public void UpdateReport(Report report)
         {
-            new FactoryDal().GetInstance().UpdateReport(report);
+            _dal.UpdateReport(report);
         }
 
         public List<Report> GetReports(Predicate<Report> predicate = null)
         {
-            return new FactoryDal().GetInstance().GetReports(predicate);
+            return _dal.GetReports(predicate);
         }
 
         public Task<List<Report>> GetReportsAsync(Predicate<Report> predicate = null)
         {
-            return new FactoryDal().GetInstance().GetReportsAsync(predicate);
+            return _dal.GetReportsAsync(predicate);
         }
 
         public Report GetReport(int? id)
         {
-            return new FactoryDal().GetInstance().GetReport(id);
+            return _dal.GetReport(id);
         }
         #endregion
     }
