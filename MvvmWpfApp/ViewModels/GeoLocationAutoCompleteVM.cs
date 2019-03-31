c2638725a7ad1beff902eb10338285aabfd69787
Neriya Bar-Lev <neriyabl555@gmail.com>
2019-03-31 17:27:08 +0300

    create autocomplite box

---
+++ MvvmWpfApp/ViewModels/GeoLocationAutoCompleteVM.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using MvvmWpfApp.Annotations;
+using MvvmWpfApp.Models;
+using QuickType;
+
+namespace MvvmWpfApp.ViewModels
+{
+    public class GeoLocationAutoCompleteVM: INotifyPropertyChanged
+    {
+        private List<Result> _locationList;
+
+        public List<Result> LocatioList
+        {
+            get { return _locationList; }
+            set
+            {
+                _locationList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private Result _selectedResult;
+
+        public Result SelectedResult
+        {
+            get { return _selectedResult; }
+            set
+            {
+                _selectedResult = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async Task AutoComp(AutoCompleteBox autoComplete)
+        {
+            var addres = autoComplete.Text;
+            var autocomplete = new GeoLocationAutoCompleteModel();
+            var resolts = await autocomplete.SearchLocation(addres);
+            if (addres != autoComplete.Text) return;
+            LocatioList = resolts;
+            autoComplete.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MvvmWpfApp/Views/GeoLocationAutoComplete.xaml.cs
@@ -0,0 +1,38 @@
+﻿using MvvmWpfApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MvvmWpfApp.Controls
+{
+    /// <summary>
+    /// Interaction logic for GeoLocationAutoComplete.xaml
+    /// </summary>
+    public partial class GeoLocationAutoComplete : UserControl
+    {
+        public GeoLocationAutoCompleteVM CompleteVM { get; set; }
+        public GeoLocationAutoComplete()
+        {
+            InitializeComponent();
+            CompleteVM = new GeoLocationAutoCompleteVM();
+            DataContext = CompleteVM;
+        }
+
+        private void AutoCompleteBox_OnTextChanged(object sender, RoutedEventArgs e)
+        {
+            CompleteVM.AutoComp(sender as AutoCompleteBox);
+        }
+
+    }
+}
